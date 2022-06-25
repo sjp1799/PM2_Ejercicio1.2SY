64e9b451576dcbb29f3fe0d874a4d38e64325d92
Sara PavonZ <sarapavon1799@example.com>
2022-06-24 22:13:36 -0600

    Ejercicio 1.2

---
+++ PM2Ejercicio1.2-master/Ejercicio12/Datos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio12
+{
+    public class Datos
+    {
+
+        public Datos()
+        {
+
+        }
+
+        public string Nombres { get; set; }
+
+        public string Apellidos { get; set; }
+
+        public string Edad { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
---
+++ PM2Ejercicio1.2-master/Ejercicio12/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio12
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnEnviar_Clicked(object sender, EventArgs e)
+        {
+            /*var name = nom.Text;
+            var ape = nom.Text;
+            var edad = nom.Text;
+            var mail = nom.Text;*/
+
+            var datos = new Datos
+            { Nombres = nom.Text, Apellidos = ape.Text, Edad = edad.Text, Email = mail.Text };
+
+            await Navigation.PushAsync(new Page2(datos));
+        }
+    }
+}
